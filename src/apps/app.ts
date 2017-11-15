2d647a77b31cc2b02ac7e51bfa8f816b0698554b
Kirk Chen <kirkchen@kkbox.com>
2017-11-15 14:42:00 +0800

    Add support for exclude instance

--- src/apps/app.ts
+++ src/apps/app.ts
@@ -1,12 +1,14 @@
 import ReservedInstanceCalculator from '../services/reservedInstanceCalculator';
 import SlackHelper from '../services/slackHelper';
 import ResourceProvider from '../providers/resourceProvider';
+import { ResourceType } from '../providers/resourceType';
 
 export default class App {
     constructor(
         private resourceProviders: ResourceProvider[],
         private reservedInstanceCalculator: ReservedInstanceCalculator,
-        private slackHelper: SlackHelper) {
+        private slackHelper: SlackHelper,
+        private excludePattern?: string) {
     }
 
     async Run(): Promise<void> {
@@ -15,16 +17,37 @@ export default class App {
             attachments: []
         };
 
+        let excludedInstances: InstanceData[] = [];
         for (let provider of this.resourceProviders) {
             let reservedInstances = await provider.describeActiveReservedInstances();
             let runningInstances = await provider.describeRunningInstances();
 
             let notReservedInstances = this.reservedInstanceCalculator.getInstanceNotReserved(reservedInstances, runningInstances);
 
+            let excludedInstancesForProvider: InstanceData[] = [];
+            notReservedInstances = notReservedInstances.filter(i => {
+                if (!this.excludePattern) {
+                    return true;
+                }
+
+                let regex = new RegExp(this.excludePattern, "g");
+                let matches = i.InstanceName!.match(regex);
+                if (matches && matches.length > 0) {
+                    excludedInstancesForProvider.push(i);
+                    return false;
+                }
+
+                return true;
+            });
+            excludedInstances = excludedInstances.concat(excludedInstancesForProvider);
+
             let slackAttachment = this.slackHelper.formatInstanceToSlackAttachment(provider.ResourceType, notReservedInstances);
             slackAttachment.footer = provider.getInstancesUrl(notReservedInstances);
             slackMessage.attachments.push(slackAttachment);
         }
+
+        let slackAttachment = this.slackHelper.formatInstanceToSlackAttachment(ResourceType.Excluded, excludedInstances);
+        slackMessage.attachments.push(slackAttachment);
         await this.slackHelper.sendToSlack(slackMessage);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export default function CheckAwsReservedInstance(): Promise<void> {
     let webhookUrl: string | undefined = process.env.RICHECKER_WEBHOOK_URL;
     let region: string = process.env.RICHECKER_REGION || 'us-east-1';
     let channel: string | undefined = process.env.RICHECKER_SLACK_CHANNEL;
+    let excludePattern: string | undefined = process.env.RICHECKER_EXCLUDE_PATTERN;
 
     if (!webhookUrl) {
         throw new Error('Webhook url is not set, please check your environment variable RICHECKER_WEBHOOK_URL');
@@ -20,7 +21,8 @@ export default function CheckAwsReservedInstance(): Promise<void> {
             new ElastiCacheProvider(region)
         ],
         new ReservedInstanceCalculator(), 
-        new SlackHelper(region, webhookUrl, channel)).Run();
+        new SlackHelper(region, webhookUrl, channel),
+        excludePattern).Run();
 
     return result;
 }
